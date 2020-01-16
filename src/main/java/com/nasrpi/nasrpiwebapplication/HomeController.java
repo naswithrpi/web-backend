c99757284425826b07223f704823b8173fffd2f4
Chinmay Kulkarni <chinmaykulkarni8@gmail.com>
2020-01-16 19:32:32 +0530

    /getAllRootItems - completed

--- src/main/java/com/nasrpi/nasrpiwebapplication/HomeController.java
+++ src/main/java/com/nasrpi/nasrpiwebapplication/HomeController.java
@@ -4,18 +4,11 @@
 
 package com.nasrpi.nasrpiwebapplication;
 
-import java.io.File;
-
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.databind.util.JSONPObject;
 
 import io.swagger.annotations.Api;
 
@@ -30,62 +23,19 @@ public class HomeController {
 
 	@Autowired
 	private StorageHandler storageHandler;
-	
+
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	public String sayHello() {
 		return ("hello");
 	}
-	
-	@RequestMapping(value = "/localStorage", method = RequestMethod.GET, produces = "application/json")
-	public String getStorageData() {
-		//String data = StorageHandler.getInstance().queryStorage();
-		//JSONObject data = queryStorage();
-		JSONObject data = storageHandler.queryStorage();
-		
-		return data.toString();
-	}
-	
-	public JSONObject queryStorage() {
-		String root_path = "E:\\";
-		/*StringBuilder files_string = new StringBuilder();
-		String [] pathnames;*/
-		
-		JSONArray jsonFiles = new JSONArray();
-		JSONObject jsonData = new JSONObject();
-		
-		File f = new File(root_path);
-		//pathnames = f.list();
-		
-		File [] files = f.listFiles();
-		
-		for (File file : files) {
-			JSONObject json = new JSONObject();
-			if(file.getName().equals("trialfile.txt")) {
-				System.out.println(file.delete());
-			}
-			try {
-				json.put("type", file.isDirectory()?"directory" : "file");
-				json.put("name", file.getName());
-				json.put("path",file.getAbsolutePath());
-				
-				JSONObject jsonPermissions = new JSONObject();
-				jsonPermissions.put("can_execute",file.canExecute());
-				jsonPermissions.put("can_read",file.canRead());
-				jsonPermissions.put("can_write",file.canWrite());
-				json.put("permissions", jsonPermissions);
-				
-				json.put("size", file.length());
-				
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			jsonFiles.put(json);
-		}
-		
-		jsonData.put("files", jsonFiles);
-		return jsonData;
-	}
 
+	@RequestMapping(value = "/getAllRootItems", method = RequestMethod.GET, produces = "application/json")
+	public String getAllRootItems() {
+		JSONObject data = storageHandler.getAllRootItems();
+		if (data != null) {
+			return data.toString();
+		} else {
+			return "{ \"error\" : \"No external device found\"}";
+		}
+	}
 }
--- src/main/java/com/nasrpi/nasrpiwebapplication/StorageHandler.java
+++ src/main/java/com/nasrpi/nasrpiwebapplication/StorageHandler.java
@@ -17,47 +17,46 @@ import io.swagger.annotations.Api;
 
 @Component
 public class StorageHandler {
-	
-	public JSONObject queryStorage() {
+
+	public JSONObject getAllRootItems() {
 		String root_path = "E:\\";
-		/*StringBuilder files_string = new StringBuilder();
-		String [] pathnames;*/
-		
+
 		JSONArray jsonFiles = new JSONArray();
 		JSONObject jsonData = new JSONObject();
-		
+
 		File f = new File(root_path);
-		//pathnames = f.list();
-		
-		File [] files = f.listFiles();
-		
-		for (File file : files) {
-			JSONObject json = new JSONObject();
-			if(file.getName().equals("trialfile.txt")) {
-				System.out.println(file.delete());
+
+		File[] files = f.listFiles();
+
+		if (files != null) {
+			for (File file : files) {
+				JSONObject json = new JSONObject();
+
+				try {
+					json.put("type", file.isDirectory() ? "directory" : "file");
+					json.put("name", file.getName());
+					json.put("path", file.getAbsolutePath());
+
+					JSONObject jsonPermissions = new JSONObject();
+					jsonPermissions.put("can_execute", file.canExecute());
+					jsonPermissions.put("can_read", file.canRead());
+					jsonPermissions.put("can_write", file.canWrite());
+					json.put("permissions", jsonPermissions);
+
+					json.put("size", file.length());
+
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+
+				jsonFiles.put(json);
 			}
-			try {
-				json.put("type", file.isDirectory()?"directory" : "file");
-				json.put("name", file.getName());
-				json.put("path",file.getAbsolutePath());
-				
-				JSONObject jsonPermissions = new JSONObject();
-				jsonPermissions.put("can_execute",file.canExecute());
-				jsonPermissions.put("can_read",file.canRead());
-				jsonPermissions.put("can_write",file.canWrite());
-				json.put("permissions", jsonPermissions);
-				
-				json.put("size", file.length());
-				
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			jsonFiles.put(json);
+
+			jsonData.put("files", jsonFiles);
+			return jsonData;
+		} else {
+			return null;
 		}
-		
-		jsonData.put("files", jsonFiles);
-		return jsonData;
+
 	}
 }
