e66f43c2ca56fc1ee979fce4443d64024142c4cd
Chinmay Kulkarni <chinmaykulkarni8@gmail.com>
2020-01-22 13:11:40 +0530

    synchronisation attempt 1

--- src/main/java/com/nasrpi/nasrpiwebapplication/SpringFoxConfig.java
+++ src/main/java/com/nasrpi/nasrpiwebapplication/SpringFoxConfig.java
@@ -5,6 +5,7 @@
 package com.nasrpi.nasrpiwebapplication;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
@@ -19,6 +20,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@ComponentScan(basePackages = "com.nasrpi")
 public class SpringFoxConfig {                                    
     @Bean
     public Docket api() { 
