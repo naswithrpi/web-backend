f8233d0226505c0d16e3097b64a724c0ac959762
Chinmay Kulkarni <chinmaykulkarni8@gmail.com>
2020-01-22 22:00:59 +0530

    C:/Program Files/Git/delete api completed

--- src/main/java/com/nasrpi/home/HomeRepository.java
+++ src/main/java/com/nasrpi/home/HomeRepository.java
@@ -1,12 +1,13 @@
 package com.nasrpi.home;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.springframework.stereotype.Repository;
@@ -15,27 +16,43 @@ import org.springframework.stereotype.Repository;
 public class HomeRepository {
 
 	public List<String> getContents(final String path) {
-		
+
 		List<GetContentsModel> getContentsList = new ArrayList<GetContentsModel>();
 
 		List<String> result = new ArrayList<>();
 
 		try (Stream<Path> walk = Files.list(Paths.get(path))) {
-			
+
 			GetContentsModel getContentsModel = new GetContentsModel();
-			
+
 			getContentsModel.setFilePath(walk.toString());
 			System.out.println("model : " + getContentsModel.getFilePath());
 
-			//result = (walk.filter(Files::isRegularFile)).map(x -> x.toString()).collect(Collectors.toList());
-			
+			// result = (walk.filter(Files::isRegularFile)).map(x ->
+			// x.toString()).collect(Collectors.toList());
+
 //			result.addAll(walk.filter(Files::isDirectory)
 //					.map(x -> x.toString()).collect(Collectors.toList()));
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return result;
 	}
+
+	public boolean delete(final String path) {
+
+		boolean isFolderDeleted = true;
+
+		try {
+			Files.walk(Paths.get(path)).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+		} catch (IOException e) {
+			e.printStackTrace();
+			isFolderDeleted = false;
+		}
+
+		return isFolderDeleted;
+	}
+
 }
