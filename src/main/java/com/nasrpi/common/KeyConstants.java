01cc1f0fb373b7c02b30e6549855783b2fdec07b
Chinmay Kulkarni <30969403+Grandolf49@users.noreply.github.com>
2020-09-16 15:55:34 +0530

    Enabled CORS for all APIs

--- src/main/java/com/nasrpi/common/KeyConstants.java
+++ src/main/java/com/nasrpi/common/KeyConstants.java
@@ -6,13 +6,13 @@ package com.nasrpi.common;
 
 /**
  * Common constants
- * 
+ *
  * @author zuilee
  */
 
 public class KeyConstants {
 
-	public static final String ROOT_PATH = "C:/";
+	public static final String ROOT_PATH = "C:\\Users\\chinm\\Desktop\\College Stuff\\NAS with Raspberry Pi\\root";
 
 	public static final String APPLICATION_JSON = "application/json";
 
--- src/main/java/com/nasrpi/nasrpiwebapplication/NasrpiWebApplication.java
+++ src/main/java/com/nasrpi/nasrpiwebapplication/NasrpiWebApplication.java
@@ -2,6 +2,9 @@ package com.nasrpi.nasrpiwebapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class NasrpiWebApplication {
@@ -10,4 +13,13 @@ public class NasrpiWebApplication {
 		SpringApplication.run(NasrpiWebApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
 }
